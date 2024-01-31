dd5f04174f3c55f0583c61d3ab889f0d9c82709e
AeonaNova <glowingghostred@gmail.com>
2024-01-31 14:13:02 +0300

    added docstrings to two functions

--- switcher.py
+++ switcher.py
@@ -3,11 +3,17 @@ import win32gui
 import os
 
 def setCyrillicLayout():
+    """
+    switch keyboard keys to cyrillic
+    """
     window_handle = win32gui.GetForegroundWindow()
     result = win32api.SendMessage(window_handle, 0x0050, 0, 0x04190419)
     return(result)
 
 def setEngLayout():
+    """
+    switch keyboard keys to english
+    """
     window_handle = win32gui.GetForegroundWindow()
     result = win32api.SendMessage(window_handle, 0x0050, 0, 0x04090409)
     return(result)
